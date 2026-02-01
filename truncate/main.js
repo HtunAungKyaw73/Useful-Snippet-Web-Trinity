75e31615085bd41dd08e5bf3d9c90ab54ef12c1b
HtunAungKyaw73 <htunaungkyaw730@gmail.com>
2026-02-01 21:03:47 +0700

    truncate

--- truncate/main.js
+++ truncate/main.js
@@ -1,1 +1,13 @@
-document.getElementById('current-year').textContent = new Date().getFullYear();
+document.getElementById("current-year").textContent = new Date().getFullYear();
+
+const length = 40;
+const text = document.querySelector(".truncate").textContent;
+const result = document.querySelector("#textBox");
+
+result.innerHTML = text.trim().substring(0, length) + "...";
+
+// const length = 20;
+// const text = document.querySelector(".truncate").textContent;
+// const result = document.querySelector("#textBox");
+
+// result.innerHTML = text.trim().substring(0, length) + "...";
